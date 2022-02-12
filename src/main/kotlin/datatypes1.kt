2a496b102cefa6864ae67d825bdac6ec3c6231fe
Winnie-Waiguru <winniewaiguru@gmail.com>
2022-02-12 08:47:27 +0300

    first commit

---
+++ src/main/kotlin/datatypes1.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var full_name: String= "Winfred Waiguru"
+    var age: Int = 30
+    var phone_number: String="0796778054"
+    var weight: String= "54 kg"
+    var areyouKenyan: Boolean = true
+
+    println(full_name)
+    println(age)
+    println(phone_number)
+    println(weight)
+    println(areyouKenyan)
+
+
+
+}
